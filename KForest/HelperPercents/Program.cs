9c047026e8b68c4055000eeac45c7cfcf7dcdaa1
Andrey Fedchuk <andrew414414@gmail.com>
2014-10-28 20:10:12 +0300

    files

---
+++ KForest/HelperPercents/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HelperPercents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double prob = 0.1;
+            StreamReader read = new StreamReader("test.csv");
+
+            Random rand = new Random(DateTime.Now.Millisecond);
+
+            StreamWriter write = new StreamWriter("test2.csv");
+            write.WriteLine(read.ReadLine());
+
+            while (!read.EndOfStream)
+            {
+                string s = read.ReadLine();
+                if (rand.NextDouble() < 0.1)
+                {
+                    write.WriteLine(s);
+                }
+            }
+
+            write.Close();
+            read.Close();
+        }
+    }
+}
---
+++ KForest/KForest/KForest.py
@@ -0,0 +1,27 @@
+import random
+import math
+import pylab as pl
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+data = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+
+#tr_features = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f20', 'f21', 'f22', 'f23', 'f24', 'f25', 'f26', 'f27', 'f28', 'f29', 'f30', 'f31', 'f32', 'f33', 'f34', 'f35', 'f36', 'f37', 'f38', 'f39', 'f40', 'f41', 'f42', 'f43', 'f44', 'f45', 'f46', 'f47', 'f48', 'f49', 'f50', 'f51', 'f52', 'f53', 'f54']
+#tr_features = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f20', 'f21', 'f22', 'f23', 'f24', 'f25', 'f26', 'f27', 'f28', 'f29', 'f30', 'f31', 'f32', 'f33', 'f34', 'f35', 'f36', 'f37', 'f38', 'f39', 'f40', 'f41', 'f42', 'f43', 'f44', 'f45', 'f46', 'f47', 'f48', 'f49', 'f50', 'f51', 'f52', 'f53', 'f54']
+tr_features = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11']
+ts_features = 'class'
+
+def knn(k, features, test_feature, train_df, test_df):
+    results = []
+
+    clf = KNeighborsClassifier(n_neighbors=k)
+        
+    clf.fit(train_df[features], train_df[test_feature])
+    preds = clf.predict(test_df[features])
+    
+    for i in preds:
+        print i
+
+knn(20, tr_features, 'class', data, test)
